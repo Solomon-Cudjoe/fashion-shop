655749bb11cd5841016f4420049bd7a2b48b7262
SOLOMON CUDJOE <scudjoe016@st.ug.edu.gh>
2025-01-01 19:16:40 +0000

    Modified the navbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,16 +4,24 @@ import styles from "../styles/NavigationBar.module.css";
 
 const NavigationBar = () => {
   return (
-    <nav className={styles.navBar}>
-      <NavLink className={styles.logo}>F</NavLink>
-      <div className={styles.navLinks}>
-        <NavLink to="/">Home</NavLink>
-
-        <NavLink to="/about">About us</NavLink>
-        {/* <NavLink>Features</NavLink> */}
-        <NavLink to="/contact">Contact us</NavLink>
-      </div>
-    </nav>
+    <header>
+      <nav>
+        <div className={styles.navBar}>
+          <div className={styles.logo}>
+            <NavLink>F</NavLink>
+          </div>
+          <div className={styles.navLinks}>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About us</NavLink>
+            <NavLink to="/features">Features</NavLink>
+            <NavLink to="/contact">Contact us</NavLink>
+          </div>
+          <div className={styles.search}>
+            <input type="search" placeholder="Search name of product..." />
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
